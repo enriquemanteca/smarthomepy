5717eb4bf050f83fc899c7145e6523e7bff540fd
enriquemanteca <uo295128@uniovi.es>
2025-12-16 14:35:50 +0100

    Green Phase of US3 (Room Occupied and Not Enough Light)

--- src/smart_room.py
+++ src/smart_room.py
@@ -53,8 +53,8 @@ class SmartRoom:
         return GPIO.input(self.PHOTO_PIN)
 
     def manage_light_level(self) -> None:
-        # To be implemented
-        pass
+        GPIO.output(self.LED_PIN, True)
+        self.light_on = True
 
     def manage_window(self) -> None:
         # To be implemented
